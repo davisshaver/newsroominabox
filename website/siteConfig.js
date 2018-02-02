6766da8e8403660d815e258249e84abb248ef957
Davis Shaver <davisshaver@gmail.com>
2018-02-02 12:59:09 -0500

    Add period to tagline

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,7 +10,7 @@ const users = [];
 
 const siteConfig = {
   title: 'Newsroom in a Box' /* title for your website */,
-  tagline: 'Open source resource for local publishers',
+  tagline: 'Open source resource for local publishers.',
   algolia: {
     apiKey: "1229c5896abf1fb0e8f9de29f271942f",
     indexName: "newsroominabox"
