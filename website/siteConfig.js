b4b31c097db48fadc2183cb570c2f13fb09f4205
Davis Shaver <davisshaver@gmail.com>
2018-02-02 12:18:20 -0500

    local & niche

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,7 +10,7 @@ const users = [];
 
 const siteConfig = {
   title: 'Newsroom in a Box' /* title for your website */,
-  tagline: 'Open source guide & tools for digital publishing',
+  tagline: 'Open source guide & tools for local & niche digital publishing',
   algolia: {
     apiKey: "1229c5896abf1fb0e8f9de29f271942f",
     indexName: "newsroominabox"
